4e4f5f35c67a67cf848f234aeec350d2a2e14558
marcos.dealmeida <blackheal@gmail.com>
2019-03-27 17:42:45 -0300

    Controller

---
+++ src/main/java/com/almeida/recipeapp/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package com.almeida.recipeapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+//@RequestMapping()
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndex() {
+        System.out.println("sup!!!!");
+        return "index";
+    }
+}
