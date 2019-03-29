d884a7bde63d08a97dd7500ae44cb184fe841a00
marcos.dealmeida <blackheal@gmail.com>
2019-03-29 16:22:00 -0300

    add enum type

--- src/main/java/com/almeida/recipeapp/domain/Recipe.java
+++ src/main/java/com/almeida/recipeapp/domain/Recipe.java
@@ -1,5 +1,7 @@
 package com.almeida.recipeapp.domain;
 
+import com.almeida.recipeapp.enums.Difficulty;
+
 import javax.persistence.*;
 import java.rmi.server.UID;
 import java.util.Set;
@@ -27,6 +29,9 @@ public class Recipe {
 
     @Lob
     private Byte image;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
@@ -95,12 +100,28 @@ public class Recipe {
         this.directions = directions;
     }
 
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
     public Byte getImage() {
         return image;
     }
 
     public void setImage(Byte image) {
         this.image = image;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 
     public Notes getNotes() {
---
+++ src/main/java/com/almeida/recipeapp/enums/Difficulty.java
@@ -0,0 +1,8 @@
+package com.almeida.recipeapp.enums;
+
+public enum Difficulty {
+    EASY,
+    MODERATE,
+    KIND_OF_HARD,
+    HARD
+}
