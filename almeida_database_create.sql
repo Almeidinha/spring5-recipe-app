7c5e707b4a834007fb1bbb313ea56979e7d00402
marcos.dealmeida <blackheal@gmail.com>
2019-04-12 13:43:49 -0300

    refactor Database initialization to mySql

---
+++ almeida_database_create.sql
@@ -0,0 +1,12 @@
+create table category (id varchar(255) not null, description varchar(255), primary key (id)) engine=InnoDB;
+create table ingredient (id varchar(255) not null, amount decimal(19,2), description varchar(255), recipe_id varchar(255), unit_of_measure_id varchar(255), primary key (id)) engine=InnoDB;
+create table notes (id varchar(255) not null, recipe_notes longtext, recipe_id varchar(255), primary key (id)) engine=InnoDB;
+create table recipe (id varchar(255) not null, cook_time integer, description varchar(255), difficulty varchar(255), directions longtext, image longblob, prep_time integer, servings integer, source varchar(255), url varchar(255), notes_id varchar(255), primary key (id)) engine=InnoDB;
+create table recipe_category (recipe_id varchar(255) not null, category_id varchar(255) not null, primary key (recipe_id, category_id)) engine=InnoDB;
+create table unit_of_measure (id varchar(255) not null, description varchar(255), primary key (id)) engine=InnoDB;
+alter table ingredient add constraint FKj0s4ywmqqqw4h5iommigh5yja foreign key (recipe_id) references recipe (id);
+alter table ingredient add constraint FK15ttfoaomqy1bbpo251fuidxw foreign key (unit_of_measure_id) references unit_of_measure (id);
+alter table notes add constraint FKdbfsiv21ocsbt63sd6fg0t3c8 foreign key (recipe_id) references recipe (id);
+alter table recipe add constraint FK37al6kcbdasgfnut9xokktie9 foreign key (notes_id) references notes (id);
+alter table recipe_category add constraint FKqsi87i8d4qqdehlv2eiwvpwb foreign key (category_id) references category (id);
+alter table recipe_category add constraint FKcqlqnvfyarhieewfeayk3v25v foreign key (recipe_id) references recipe (id);
